d2a3c8e51b9dd83a529d6d414d60a617be274c31
EloraDelanes <eloraginish@gmail.com>
2019-12-16 13:34:57 +0100

    Prodject tincat

--- functions/setUser.php
+++ functions/setUser.php
@@ -11,10 +11,40 @@ try {
     print "Erreur !: " . $e->getMessage() . "<br/>";
     die();
 }
-// Etape 3: preparer une request
-$prenom = "Elo";
+//Etape 3 : prepare request
+$req = $db->prepare("INSERT INTO users (pseudo, email, password) VALUES(:pseudo, :email, :password)");
 
-$req = $db->prepare("INSERT INTO users (pseudo, password) VALUES(:pseudo, :password)");
-$req->bindParam(":pseudo", $prenom);
-$req->bindParam(":password", $prenom);
-$req->execute();
+// Avant d'insérer en base de données faire les vérifications suivantes
+    // Vérifier si le pseudo ou le mot de passe est vide
+    if($_POST["pseudo"] == "" || $_POST["password"] == "" || $_POST["email"] == ""){
+    echo "Veuillez renseigner tous les champs.";
+    //Redirection vers la page register.php
+    header("Location: ../register.php?inputNone=Veuillez renseigner tous les champs.");
+    }
+    else {
+        $req->bindParam(":pseudo", $_POST["pseudo"]);
+    }
+
+    // Ajouter un input confirm password et vérifier si les deux sont égaux
+    
+    if($_POST["password"] === $_POST["confirmp"]){
+        $req->bindParam(":password", $_POST["password"]);
+    }
+    else{
+        echo "Veuillez renseigner les même mots de passe.";
+    //Redirection vers la page register.php
+    header("Location: ../register.php?errorPassword=Veuillez renseigner les même mots de passe.");
+    }
+
+
+// Ajouter un champ email
+$req->bindParam(":email", $_POST["email"]);
+
+var_dump($_POST);
+
+if($req->execute()){
+    echo "Inscription valider.";
+    //Redirection vers la page register.php
+    header("Location: ../register.php?errorPassword=Inscription valider.");
+}
+
--- register.php
+++ register.php
@@ -1,13 +1,31 @@
-<?php require "head.php"; ?>
-<body>
-    <div class="form-container">
-        <h1>TINCAT</h1>
-        <form action="functions/setUser.php" method="post">
-            <input type="text" placeholder="pseudo" name="pseudo">
-            <input type="password" placeholder="password" name="password">
-            <input type="submit" value="register">
-        </form>
-    </div>
 
+<?php require "head.php" ?>
+<body>
+    <div class="fond"></div>
+    <div class="form-container">
+    <a href="index.php"><h1>TINCAT</h1></a> 
+    <form action="functions/setUser.php" method="post">
+        <input type="text" placeholder="Pseudo" name="pseudo">
+        <input type="email" placeholder="Email" name="email">
+        <input type="password" placeholder="Password" name="password">
+        <input type="password" placeholder="Confirm Password" name="confirmp">
+        <input type="submit" placeholder="Envoyer" value="Register">
+    </form>
+    <?php
+        //echos erreurs succès
+        if(isset($_GET["inputNone"])){
+            echo "<div class=\"error\">";
+            echo $_GET["inputNone"];
+            echo "</div>";
+        }
+        else if(isset($_GET["errorPassword"])){
+            echo "<div class=\"error\">";
+            echo $_GET["errorPassword"];
+            echo "</div>";
+        }
+        ?>
+
+        <a href="login.php" class="link">Connexion</a>
+    </div>
 </body>
 </html>
